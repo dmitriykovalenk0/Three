38e8a6f95564e13b19e922e13bd591e03cfe19a1
dmitriykovalenk0 <dimakovalenko1999god@gmail.com>
2019-11-25 12:45:46 +0200

    Создан блок featured

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
-$(function(){
+$(function () {
+
+  $('.slider').slick({
+    nextArrow: '<img class="nextArrow" src="img/arrow-right.svg" alt="arrow-right">',
+    prevArrow: '<img class="prevArrow" src="img/arrow-left.svg" alt="arrow-left">'
+
+  })
 
 
 
-  
+
 });
