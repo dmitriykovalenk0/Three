1872a6f5bfeaad864e70447aa5a312d7357ada4a
dmitriykovalenk0 <dimakovalenko1999god@gmail.com>
2019-11-25 22:23:59 +0200

    Создан блок team

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,16 @@
 $(function () {
 
   $('.slider').slick({
-    nextArrow: '<img class="nextArrow" src="img/arrow-right.svg" alt="arrow-right">',
-    prevArrow: '<img class="prevArrow" src="img/arrow-left.svg" alt="arrow-left">'
+    nextArrow: '<img class="nextArrow feature__slider-nextArrow" src="img/arrow-right.svg" alt="arrow-right">',
+    prevArrow: '<img class="prevArrow feature__slider-prevArrow" src="img/arrow-left.svg" alt="arrow-left">'
+  })
 
+  $('.team__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: '<img class="nextArrow team__slider-nextArrow" src="img/arrow-right.svg" alt="arrow-right">',
+    prevArrow: '<img class="prevArrow team__slider-prevArrow" src="img/arrow-left.svg" alt="arrow-left">'
   })
 
 
