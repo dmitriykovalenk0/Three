30e574e0a638eeed873c373495cf9b7b3a912417
dmitriykovalenk0 <dimakovalenko1999god@gmail.com>
2019-11-27 22:11:21 +0200

    Создан адаптив для блоков header featured projects

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function () {
 
   $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     nextArrow: '<img class="nextArrow feature__slider-nextArrow" src="img/arrow-right.svg" alt="arrow-right">',
     prevArrow: '<img class="prevArrow feature__slider-prevArrow" src="img/arrow-left.svg" alt="arrow-left">'
   })
